8c56ff67a4b909f694a19c85fd108bab68086490
Evers,Paul P.H.A <paulusman@live.nl>
2020-10-08 11:02:25 +0200

    Components

---
+++ DxdPortfolio/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  @ViewChild('navBurger', {static: false}) navBurger: ElementRef;
+  @ViewChild('navMenu', {static: false}) navMenu: ElementRef;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  toggleNavbar() {
+    this.navBurger.nativeElement.classList.toggle('is-active');
+    this.navMenu.nativeElement.classList.toggle('is-active');
+  }
+
+}
