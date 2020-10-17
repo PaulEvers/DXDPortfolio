f07767b039aac9d84fd2de597e1152455b576947
Paul <paulusman@live.nl>
2020-10-17 16:26:10 +0200

    More content

--- DxdPortfolio/src/app/vega/vega.module.ts
+++ DxdPortfolio/src/app/vega/vega.module.ts
@@ -4,12 +4,15 @@ import { CommonModule } from '@angular/common';
 import { VegaRoutingModule } from './vega-routing.module';
 import { VegaComponent } from './vega.component';
 
+import { MatExpansionModule } from '@angular/material/expansion';
+
 
 @NgModule({
   declarations: [VegaComponent],
   imports: [
     CommonModule,
-    VegaRoutingModule
+    VegaRoutingModule,
+    MatExpansionModule
   ]
 })
 export class VegaModule { }
