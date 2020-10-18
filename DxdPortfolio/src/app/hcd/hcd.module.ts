f22f45660740aff5e4a59ba5b6b289917a2dc835
Evers,Paul P.H.A <paulusman@live.nl>
2020-10-18 15:30:16 +0200

    HCD

--- DxdPortfolio/src/app/hcd/hcd.module.ts
+++ DxdPortfolio/src/app/hcd/hcd.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HcdRoutingModule } from './hcd-routing.module';
 import { HcdComponent } from './hcd.component';
+import { MatExpansionModule } from '@angular/material';
 
 
 @NgModule({
   declarations: [HcdComponent],
   imports: [
     CommonModule,
-    HcdRoutingModule
+    HcdRoutingModule,
+    MatExpansionModule
   ]
 })
 export class HcdModule { }
